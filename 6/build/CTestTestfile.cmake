42cd4aad7f011273934c6b8a54cad87fd224e7e3
Артем Игнатьев <ogzeus@MacBook-Pro-Artem-2.local>
2025-11-29 22:03:34 +0300

    delete build

--- 6/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ogzeus/OOP-207-MAI-Autumn/oop_mai/6
-# Build directory: /Users/ogzeus/OOP-207-MAI-Autumn/oop_mai/6/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(DungeonTests "/Users/ogzeus/OOP-207-MAI-Autumn/oop_mai/6/build/lab_tests")
-set_tests_properties(DungeonTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/ogzeus/OOP-207-MAI-Autumn/oop_mai/6/CMakeLists.txt;44;add_test;/Users/ogzeus/OOP-207-MAI-Autumn/oop_mai/6/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
